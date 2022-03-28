4cf794f48cb0cbc48dde3d2acaeace5cee8d558c
Benito Geordie <benito@thirdai.com>
2022-03-28 16:30:26 +0000

    demo

--- interactive_sentiment_analysis.py
+++ interactive_sentiment_analysis.py
@@ -2,7 +2,7 @@ from thirdai import bolt
 import numpy as np
 import mmh3
 
-def demo(network: bolt.Network):
+def demo(network: bolt.Network, verbose: bool):
     include_bigrams = True
     k = 0
     seed = 0
@@ -10,6 +10,9 @@ def demo(network: bolt.Network):
     while True:
         inp = input('Input:')
         line = inp.lower().strip()
+        if line == "exit":
+            print("Exiting demo...")
+            break
         itms = line.split(' ')
         ## we can add stemming, lower casing etc here
         x_idxs = [mmh3.hash(itm, seed) % feat_hash_dim for itm in itms]
@@ -19,11 +22,18 @@ def demo(network: bolt.Network):
             x_idxs += [mmh3.hash(line[j:j+k], seed)%feat_hash_dim for j in range(len(line)+1-k)]
         ##
         x_offsets = np.int32([0,len(x_idxs)])
-        label = np.array([np.uint32(0)])
         x_vals = np.ones(len(x_idxs))
+        y_idxs = np.array([np.uint32(0)])
+        y_vals = np.array([np.float32(1)])
+        y_offsets = np.array([np.uint32(10), np.uint32(10)])
         ######
-        temp = network.predict(x_idxs, x_vals, x_offsets, label, [0,1], batch_size=1)
-        if temp>0:
-            print('positive!')
+        temp = network.predict(
+            x_idxs, x_vals, x_offsets, 
+            y_idxs, y_vals, y_offsets, 
+            batch_size=1, 
+            metrics=["categorical_accuracy"])
+        pred = np.argmax(temp[1][0])
+        if pred > 0:
+            print('positive!', flush=True)
         else:
-            print('negative!')
+            print('negative!', flush=True)
