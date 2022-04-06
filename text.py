fa61a11845ae5baa31c16501888f8cd48b1e68a6
BenGeordie <school@bengeordie@gmail.com>
2022-04-05 23:01:20 -0500

    includes preprocessor!

--- text.py
+++ text.py
@@ -4,8 +4,8 @@ import csv
 import mmh3
 import re
 
-def tokenize_to_svm(file_name, target_location=None, train=True, seed=42):
-    if file_name.find(".csv") == -1:
+def tokenize_to_svm(filename, output_dim, target_location=None, train=True, seed=42):
+    if filename.find(".csv") == -1:
         raise ValueError("Only .csv files are supported")
 
     post = "_train" if train else "_test"
@@ -14,7 +14,7 @@ def tokenize_to_svm(file_name, target_location=None, train=True, seed=42):
     target_location = target_location + post + ".svm"
 
     fw = open(target_location, "w")
-    csvreader = csv.reader(open(file_name, "r"))
+    csvreader = csv.reader(open(filename, "r"))
 
     for line in csvreader:
         label = 1 if line[0] == "pos" else 0
@@ -23,7 +23,7 @@ def tokenize_to_svm(file_name, target_location=None, train=True, seed=42):
         sentence = re.sub(r"[^\w\s]", "", line[1])
         sentence = sentence.lower()
         ### BOLT TOKENIZER START
-        tup = dataset.bolt_tokenizer(sentence)
+        tup = dataset.bolt_tokenizer(sentence, seed=seed, dim=output_dim)
         for idx, val in zip(tup[0], tup[1]):
             fw.write(str(idx) + ":" + str(val) + " ")
         ### BOLT TOKENIZER END
