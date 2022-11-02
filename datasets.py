fe74736c2401c65c603d5c06a02477fa9d6d4f56
BenGeordie <school@bengeordie@gmail.com>
2022-11-02 01:47:42 -0500

    dataset.py

---
+++ datasets.py
@@ -0,0 +1,89 @@
+import os
+import pandas as pd
+import zipfile
+import json
+
+
+def _download_dataset(url, zip_file, check_existence, output_dir):
+    if not os.path.exists(zip_file):
+        os.system(
+            f"curl {url} --output {zip_file}"
+        )
+
+    if not os.path.exists(check_existence):
+        with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+            zip_ref.extractall(output_dir)
+
+
+def to_batch(dataframe):
+    return [
+        {
+            col_name: str(col_value) 
+            for col_name, col_value in record.items()
+        } 
+        for record in dataframe.to_dict(orient='records')
+    ]
+
+
+
+def download_movielens():
+    MOVIELENS_1M_URL = "https://files.grouplens.org/datasets/movielens/ml-1m.zip"
+    MOVIELENS_ZIP = "./movielens.zip"
+    MOVIELENS_DIR = "./movielens"
+    RATINGS_FILE = MOVIELENS_DIR + "/ml-1m/ratings.dat"
+    TRAIN_FILE = "./movielens_train.csv"
+    TEST_FILE = "./movielens_test.csv"
+    SPLIT = 0.9
+    INFERENCE_BATCH_SIZE = 5
+
+    _download_dataset(url=MOVIELENS_1M_URL, zip_file=MOVIELENS_ZIP, check_existence=RATINGS_FILE, output_dir=MOVIELENS_DIR)
+    
+    df = pd.read_csv(RATINGS_FILE, header=None, delimiter='::')
+    df.columns = ["userId", "movieId", "rating", "timestamp"]
+    df["timestamp"] = pd.to_datetime(df["timestamp"], unit='s')
+    df = df[["userId", "movieId", "timestamp"]]
+
+    n_train_samples = int(SPLIT * len(df) - INFERENCE_BATCH_SIZE)
+    train_df = df.iloc[:n_train_samples]
+    test_df = df.iloc[n_train_samples:-INFERENCE_BATCH_SIZE]
+    train_df.to_csv(TRAIN_FILE, index=False)
+    test_df.to_csv(TEST_FILE, index=False)
+
+    index_batch = to_batch(df.iloc[-INFERENCE_BATCH_SIZE:])
+    inference_batch = to_batch(df[["userId", "timestamp"]].iloc[-INFERENCE_BATCH_SIZE:])
+
+    return TRAIN_FILE, TEST_FILE, inference_batch, index_batch
+
+def download_clinc():
+    CLINC_URL = "https://archive.ics.uci.edu/ml/machine-learning-databases/00570/clinc150_uci.zip"
+    CLINC_ZIP = "./clinc150_uci.zip"
+    CLINC_DIR = "./clinc"
+    MAIN_FILE = CLINC_DIR + "/clinc150_uci/data_full.json"
+    TRAIN_FILE = "./clinc_train.csv"
+    TEST_FILE = "./clinc_test.csv"
+    INFERENCE_BATCH_SIZE = 5
+
+    _download_dataset(url=CLINC_URL, zip_file=CLINC_ZIP, check_existence=MAIN_FILE, output_dir=CLINC_DIR)
+
+    samples = json.load(open(MAIN_FILE))
+
+    train_samples = samples["train"]
+    test_samples = samples["test"]
+
+    train_text, train_category = zip(*train_samples)
+    test_text, test_category = zip(*test_samples)
+
+    train_df = pd.DataFrame({"text": train_text, "category": train_category})
+    test_df = pd.DataFrame({"text": test_text, "category": test_category})
+
+    train_df["text"] = train_df["text"].apply(lambda x: x.replace(",", ""))
+    test_df["text"] = test_df["text"].apply(lambda x: x.replace(",", ""))
+
+    train_df.to_csv(TRAIN_FILE, index=False)
+    test_df.to_csv(TEST_FILE, index=False)
+
+    inference_batch = to_batch(test_df[["text"]].sample(frac=1).iloc[:INFERENCE_BATCH_SIZE])
+
+    return TRAIN_FILE, TEST_FILE, inference_batch
+
+    
