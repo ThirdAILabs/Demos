74afe5d69c67ac175c0f0ddea9cdd1f66c30d04f
Benito Geordie <benito@thirdai.com>
2022-11-02 19:57:08 -0500

    Fixed personalization demo and utils

--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ def _download_dataset(url, zip_file, check_existence, output_dir):
             f"curl {url} --output {zip_file}"
         )
 
-    if not os.path.exists(check_existence):
+    if any([not os.path.exists(must_exist) for must_exist in check_existence]):
         with zipfile.ZipFile(zip_file, 'r') as zip_ref:
             zip_ref.extractall(output_dir)
 
@@ -31,29 +31,37 @@ def download_movielens():
     MOVIELENS_ZIP = "./movielens.zip"
     MOVIELENS_DIR = "./movielens"
     RATINGS_FILE = MOVIELENS_DIR + "/ml-1m/ratings.dat"
+    MOVIE_TITLES = MOVIELENS_DIR + "/ml-1m/movies.dat"
     TRAIN_FILE = "./movielens_train.csv"
     TEST_FILE = "./movielens_test.csv"
     SPLIT = 0.9
     INFERENCE_BATCH_SIZE = 5
 
-    _download_dataset(url=MOVIELENS_1M_URL, zip_file=MOVIELENS_ZIP, check_existence=RATINGS_FILE, output_dir=MOVIELENS_DIR)
+    _download_dataset(url=MOVIELENS_1M_URL, zip_file=MOVIELENS_ZIP, check_existence=[RATINGS_FILE], output_dir=MOVIELENS_DIR)
     
     df = pd.read_csv(RATINGS_FILE, header=None, delimiter='::', engine='python')
     df.columns = ["userId", "movieId", "rating", "timestamp"]
     df["timestamp"] = pd.to_datetime(df["timestamp"], unit='s')
-    df = df.sort_values("timestamp")
-    df = df[["userId", "movieId", "timestamp"]]
 
-    n_train_samples = int(SPLIT * len(df) - INFERENCE_BATCH_SIZE)
+    movies_df = pd.read_csv(MOVIE_TITLES, header=None, delimiter='::', engine='python', encoding="ISO-8859-1")
+    movies_df.columns = ["movieId", "movieTitle", "genre"]
+    movies_df["movieTitle"] = movies_df["movieTitle"].apply(lambda x: x.replace(",", ""))
+
+    df = pd.merge(df, movies_df, on='movieId')
+    df = df[["userId", "movieTitle", "timestamp"]]
+    df = df.sort_values("timestamp")
+
+    n_train_samples = int(SPLIT * len(df))
     train_df = df.iloc[:n_train_samples]
-    test_df = df.iloc[n_train_samples:-INFERENCE_BATCH_SIZE]
+    test_df = df.iloc[n_train_samples:]
     train_df.to_csv(TRAIN_FILE, index=False)
     test_df.to_csv(TEST_FILE, index=False)
 
-    index_batch = to_batch(df.iloc[-INFERENCE_BATCH_SIZE:])
-    inference_batch = to_batch(df[["userId", "timestamp"]].iloc[-INFERENCE_BATCH_SIZE:])
+    index_batch = to_batch(df.iloc[:INFERENCE_BATCH_SIZE])
+    inference_batch = to_batch(df.iloc[:INFERENCE_BATCH_SIZE][["userId", "timestamp"]])
 
     return TRAIN_FILE, TEST_FILE, inference_batch, index_batch
+
 
 def download_clinc():
     CLINC_URL = "https://archive.ics.uci.edu/ml/machine-learning-databases/00570/clinc150_uci.zip"
@@ -64,7 +72,7 @@ def download_clinc():
     TEST_FILE = "./clinc_test.csv"
     INFERENCE_BATCH_SIZE = 5
 
-    _download_dataset(url=CLINC_URL, zip_file=CLINC_ZIP, check_existence=MAIN_FILE, output_dir=CLINC_DIR)
+    _download_dataset(url=CLINC_URL, zip_file=CLINC_ZIP, check_existence=[MAIN_FILE], output_dir=CLINC_DIR)
 
     samples = json.load(open(MAIN_FILE))
 
