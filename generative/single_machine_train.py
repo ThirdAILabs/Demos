2898381284439e7e7feffbf961083ee0ab85d14a
Pratik Pranav <pratik@thirdai.com>
2024-04-12 07:26:28 +0000

    adds-changes-for-7b

--- generative/single_machine_train.py
+++ generative/single_machine_train.py
@@ -1,15 +1,32 @@
-# This script is for further pretraining, where each datapoint is {"target": <text>}, see sample dataset
 # For further finetuning on downstream task, where it is required to have a context. the format should
-# be {"context": <context text>,"target": <target text>}
-
+# for Bolt-7B
+# be {"prompt": <prompt text>, "context": <context text>,"target": <target text>}
 from thirdai import bolt, dataset, licensing
+import argparse
 
+parser = argparse.ArgumentParser(description="Model Finetuning Parameters")
+
+
+parser.add_argument(
+    "--learning_rate",
+    type=float,
+    required=True,
+    help="Learning rate for the model training",
+)
+parser.add_argument(
+    "--epochs", type=int, required=True, help="Number of epochs for the model training"
+)
+parser.add_argument(
+    "--batch_size", type=int, required=True, help="Batch size per worker"
+)
+
+args = parser.parse_args()
 
 # Please register for a free license at https://www.thirdai.com/try-bolt/
 licensing.activate("")
 
-train_filename = ""  # train file path
-val_filename = ""  # validation file path
+train_filename = ""
+val_filename = ""
 MODEL_PATH = ""
 SAVE_PATH = ""
 
@@ -20,9 +37,10 @@ model = bolt.GenerativeModel.load(MODEL_PATH)
 
 model.train(
     train_data=train_data,
-    epochs=5,
-    batch_size=10_000,
-    learning_rate=0.00001,
+    epochs=args.epochs,
+    batch_size=args.batch_size,
+    learning_rate=args.learning_rate,
+    max_in_memory_batches=10,  # Change as per your memory availablity
     train_metrics=["loss"],
     val_data=val_data,
     val_metrics=["loss"],
